564ec6cfba3c57be82d9b2b194b119d4c82a35e6
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2023-11-15 04:01:34 +0100

    v3.7

--- improved_fps_booster/lua/improved_fps_booster/improved_fps_booster_server/improved_fps_booster_server.lua
+++ improved_fps_booster/lua/improved_fps_booster/improved_fps_booster_server/improved_fps_booster_server.lua
@@ -7,19 +7,13 @@
 ------------- GNU General Public License v3.0
 ------------- https://github.com/Inj3-GT
 --\\--
-do
-    local ipr_addfile = {"resource/fonts/Rajdhani-Bold.ttf", "materials/icon/ipr_boost_computer.png", "materials/icon/ipr_boost_wrench.png"}
-    for _, v in pairs(ipr_addfile) do
-        resource.AddFile(v)
-    end
-end
-
 local function IprFpsBooster_Init()
     local ipr_tbl = {"HostnameThink", "CheckHookTimes"}
     for _, v in pairs(ipr_tbl) do
         if not timer.Exists(v) then
             continue
         end
+        
         timer.Remove(v)
     end
 
