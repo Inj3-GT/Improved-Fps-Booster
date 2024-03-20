052fe042981e6eb5cb5fc9aac9d311874a4145ea
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2024-03-21 00:21:37 +0100

    v2.8

--- improved_fps_booster/lua/improved_fps_booster/improved_fps_booster_server/improved_fps_booster_server.lua
+++ improved_fps_booster/lua/improved_fps_booster/improved_fps_booster_server/improved_fps_booster_server.lua
@@ -2,21 +2,11 @@
 ----------- // SCRIPT BY INJ3 
 ----------- // SCRIPT BY INJ3 
 ---- // https://steamcommunity.com/id/Inj3/
-
---//--
 ------------- GNU General Public License v3.0
 ------------- https://github.com/Inj3-GT
---\\--
-local function IprFpsBooster_Init()
-    local ipr_tbl = {"HostnameThink", "CheckHookTimes"}
-    for _, v in pairs(ipr_tbl) do
-        if not timer.Exists(v) then
-            continue
-        end
-        
-        timer.Remove(v)
-    end
 
-    hook.Remove("Think", "CheckSchedules")
-end
-hook.Add("Initialize", "IprFpsBooster_Init", IprFpsBooster_Init)
+hook.Add("Initialize", "Ipr_FpsBooster_Initialize", function()
+    if (timer.Exists("HostnameThink")) then
+        timer.Remove("HostnameThink")
+    end
+end)
