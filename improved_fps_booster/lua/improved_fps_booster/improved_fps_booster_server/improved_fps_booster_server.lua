4cbc7d7bd1fc604661402a25370dec0c579dddc1
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2024-03-21 00:55:32 +0100

    v3.8

--- improved_fps_booster/lua/improved_fps_booster/improved_fps_booster_server/improved_fps_booster_server.lua
+++ improved_fps_booster/lua/improved_fps_booster/improved_fps_booster_server/improved_fps_booster_server.lua
@@ -4,7 +4,6 @@
 ---- // https://steamcommunity.com/id/Inj3/
 ------------- GNU General Public License v3.0
 ------------- https://github.com/Inj3-GT
-
 hook.Add("Initialize", "Ipr_FpsBooster_Initialize", function()
     if (timer.Exists("HostnameThink")) then
         timer.Remove("HostnameThink")
