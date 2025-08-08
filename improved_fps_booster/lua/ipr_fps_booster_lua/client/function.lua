e1cb2cab60797587ae826f6feb23606f7a21c274
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2025-08-08 18:34:38 +0200

    v4.0.1

--- improved_fps_booster/lua/ipr_fps_booster_lua/client/function.lua
+++ improved_fps_booster/lua/ipr_fps_booster_lua/client/function.lua
@@ -228,6 +228,7 @@ Ipr.Function.FpsCalculator = function()
 
     if Ipr_SysTime > (Ipr.CurNext or 0) then
         local Ipr_AbsoluteFrameTime = engine.AbsoluteFrameTime()
+        
         Ipr.Settings.FpsCurrent = math.Round(1 / Ipr_AbsoluteFrameTime)
         Ipr.Settings.FpsCurrent = (Ipr.Settings.FpsCurrent > Ipr.Settings.MaxFps) and Ipr.Settings.MaxFps or Ipr.Settings.FpsCurrent
 
@@ -610,3 +611,4 @@ Ipr.Function.SettingsVgui = {
 }
 
 return Ipr
+
